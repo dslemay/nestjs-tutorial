1d598ccf9d136c6977ef213e592b0be017df9c3d
Daniel Lemay <26767995+dslemay@users.noreply.github.com>
2021-01-21 10:21:15 -0800

    add data transformation to validation pipe

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      forbidNonWhitelisted: true, // Throw an error if params are passed that are not part of the DTO
+      whitelist: true, // Automatically strip out params that don't adhere to the DTO
+      transform: true // automatically convert req body to instance of DTO classes
+    }),
+  );
   await app.listen(5000);
 }
 bootstrap();

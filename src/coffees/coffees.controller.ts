f10c7a6f7b58a9cdcf95ccd2546fb83209784ac2
Daniel Lemay <26767995+dslemay@users.noreply.github.com>
2021-03-26 15:56:34 -0700

    refine guards using metadata and custom decorators

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -13,12 +13,14 @@ import { PaginationQueryDto } from '../common/dto/pagination-query.dto';
 import { CoffeesService } from './coffees.service';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
+import { Public } from '../common/decorators/public.decorator';
 
 @Controller('coffees')
 export class CoffeesController {
   constructor(private readonly coffeesService: CoffeesService) {}
 
   @Get()
+  @Public()
   findAll(@Query() paginationQuery: PaginationQueryDto) {
     return this.coffeesService.findAll(paginationQuery);
   }
